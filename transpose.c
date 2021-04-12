fc3bcbebf5310a1f21b242129a9ecef317c6f8b4
NogNoa <nngnnadas@gmail.com>
2021-04-12 18:40:00 +0300

    I'm doing something here

--- transpose.c
+++ transpose.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
-#define IN 1 /* inside a word */
-#define OUT 0 /* outside a word */
+#define MAX 1024
 
-int line_count()
-{
-	int c, nl;
-	nl = 0;
-	while ((c = getchar()) != EOF)
-		if (c == '\n')
-			++nl;
-	printf("%d\n", nl);
+void linearise(char * lini[])
+int KnR_getline(char s[], int lim)
+void expose(char * lini[])
+
+int KnR_getline(char s[], int lim)
+{  /* read a line into s, return lenght
+    imported as is from longest_line.c*/
+
+	int c, i;
+
+	for (i=0; i < lim-1 && (c=getchar()) !=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c== '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
 }
+
+void expose(char * lini[], nline)
+{
+    char line[Max]= '\0';
+
+    do{
+        for (i=0; i <= nline; ++i){
+            for (j=0)
+        }
+
+        }while (line != '\0')
+}
+
+
+void linearise(char * lini[])
+    int len, i;
+    char line[MAX];
+
+    for (i=0; (len = KnR_getline(line, MAX)) > 0; ++i)
+        lini[i] = line;
+    return (i)
+
 
 void main()
 {
-	int c, state;
-	state = OUT;
-	while ((c = getchar()) != EOF) {
-		if (c == ' ' || c == '\n' || c == '\t'){
-			if (state == IN){
-				putchar('\n');
-				state = OUT;
-			}
-		}
-		else {
-			state = IN;
-			putchar(c);
-		}
-	}
-	putchar('\n');
+    char * lini[MAX];
+    int nline;
+
+    nline = liearise(lini);
+
 }
