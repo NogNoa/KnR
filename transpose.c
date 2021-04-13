f69988d55e512b8b8a747345396648da2d3f7113
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-04-13 22:15:23 +0300

    maybe transposing text in this language is more complex than I first thought

--- transpose.c
+++ transpose.c
@@ -5,6 +5,11 @@ void linearise(char * lini[])
 int KnR_getline(char s[], int lim)
 void expose(char * lini[])
 
+/*
+Text is nline lines, each killed by /0.
+so we somehow also has to follow which lines are dead. this is beyond me.
+*/
+
 int KnR_getline(char s[], int lim)
 {  /* read a line into s, return lenght
     imported as is from longest_line.c*/
@@ -23,7 +28,7 @@ int KnR_getline(char s[], int lim)
 
 void expose(char * lini[], nline)
 {
-    char line[Max]= '\0';
+    char line[MAX];
 
     do{
         for (i=0; i <= nline; ++i){
@@ -37,10 +42,11 @@ void expose(char * lini[], nline)
 void linearise(char * lini[])
     int len, i;
     char line[MAX];
+	int mxlen = 0;
 
     for (i=0; (len = KnR_getline(line, MAX)) > 0; ++i)
         lini[i] = line;
-    return (i)
+    return i
 
 
 void main()
