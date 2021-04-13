5c82c13abe8cf9913d9847c87f0d213f34bde05e
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-04-13 22:41:36 +0300

    brian k. asked
    maybe more sophisticated. celsius's a function. Moved variables into main. made while 3-line init-while-step into 1-line for.

--- centigrise.c
+++ centigrise.c
@@ -1,18 +1,18 @@
-#include <cstdio>
-int fahr;
-float lower, upper, step, celsius;
+#include <stdio.h>
 
-main()
+double celsius(int fahr)
 {
+	return (5.0/9.0) * (fahr-32);
+}
+
+void main()
+{
+	int fahr;
+	float lower, upper, step;
     lower = 0;
     upper = 300;
     step = 20;
 
-    fahr = lower;
-    while (fahr <= upper)
-    {
-        celsius = (5.0/9.0) * (fahr-32);
-        printf("%3df %6.1fc\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for (fahr = lower; fahr <= upper; fahr = fahr + step)
+        printf("%3df %6.1fc\n", fahr, celsius(fahr));;
 }
