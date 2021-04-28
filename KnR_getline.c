3c59c9294d1d6d75345b596613dec3034de9c64b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-04-28 17:56:15 +0300

    pascal string line

--- KnR_getline.c
+++ KnR_getline.c
@@ -15,8 +15,22 @@ int KnR_getline(char s[], int lim)
 }
 
 void copy(char to[], char from[])
-{  // copy 'from' into 'to'; assume 'to' is big enough */
+{  // copy 'from' into 'to'; assume 'to' is big enough
 
 	for (int i = 0;(to[i] = from[i]) != '\0'; ++i)
 		;
+
+}
+
+void pascal_getline(char s[], int lim)
+{ // give a line with length in the begining
+		// high byte is ones-complement, hence breaks at lengths > 0xFEFF = 65279
+
+	short len = (short) KnR_getline(s, lim);
+
+	for (int i=len;i>=0;--i){
+		s[i+2] = s[i];
+	}
+	s[1] = len & 0xFF;
+	s[0] = (len >> 8) ^ 0xFF ;
 }
