9f90d6f39a4ef20d39b10aa60f4c69b4cb67f340
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-04-30 12:56:43 +0300

    quick sort algorithm

---
+++ qsort.c
@@ -0,0 +1,30 @@
+
+void swap(int v[], int i, int j)
+{	/* swap: interchange v[i] and v[j] */
+
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+void qsort(int v[], int left, int right)
+{	/* qsort: sort v[left]...v[right] into increasing order */
+
+	int i, last;
+	void swap(int v[], int i, int j);
+
+	/* do nothing if array contains fewer than two elements */
+	if (left >= right) 
+		return;
+	/* move partition elem to v[0] */
+	swap(v, left, (left + right)/2); 
+	last = left; 
+	for (i = left + 1; i <= right; i++) /* partition */
+		if (v[i] < v[left])
+			swap(v, ++last, i);
+	swap(v, left, last); /* restore partition elem */
+	qsort(v, left, last-1);
+	qsort(v, last+1, right);
+}
