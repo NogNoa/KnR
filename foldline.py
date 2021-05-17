2610ecfe4a85d59103442aad2e4a10ad694c5ab3
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-05-17 13:14:54 +0300

    fix python implementation

--- foldline.py
+++ foldline.py
@@ -2,6 +2,13 @@
 LNSIZ = 0x50
 
 
+def equ_fold(string: str):
+    print(string[:LNSIZ], end='')
+    if len(string) > LNSIZ:
+        print('\n')
+        smrt_fold(string[LNSIZ:])
+
+
 def smrt_fold(string: str):
     IN = 1
     OUT = 0
@@ -9,42 +16,46 @@ def smrt_fold(string: str):
     last = 0
 
     for j in range(len(string)):
-        if string[j] in {' ', '\t'}:
+        if j >= LNSIZ:
+            break
+        elif string[j] in {' ', '\t', '\n'}:
             state = OUT
         elif state == OUT:
-            if j >= LNSIZ:
-                break
             last = j
             state = IN
-
-    print(string[:last] + "\n")
-    if string[j]:
-        smrt_fold(string[last:])
+    if not last:
+        equ_fold(string)
+    else:
+        print(string[:last])
+        if j <= LNSIZ:
+            smrt_fold(string[last:])
+        else:
+            print(string[last:])
 
 
 """
 void equ_fold (char str[])
 {
-	int j;
+   int j;
 
-	for (j=0;j<LNSIZ && str[j] ;++j)
-		{putchar(str[j]);}
-	putchar('\n');
-	if (str[j])
-		equ_fold(&str[LNSIZ]);
+   for (j=0;j<LNSIZ && str[j] ;++j)
+      {putchar(str[j]);}
+   putchar('\n');
+   if (str[j])
+      equ_fold(&str[LNSIZ]);
 }
 
 
 def main()
-	char line[MAXLINE];
+   char line[MAXLINE];
 
-	while ((len = KnR_getline(line, MAXLINE)) > 0){
-		if (len <= LNSIZ)
-			{printf("%s", line);}
-		else 
-			{smrt_fold(line);}
-	}
-	return 0;
+   while ((len = KnR_getline(line, MAXLINE)) > 0){
+      if (len <= LNSIZ)
+         {printf("%s", line);}
+      else 
+         {smrt_fold(line);}
+   }
+   return 0;
 """
 
 smrt_fold("       <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\"://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>\
