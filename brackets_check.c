8121d27dfb08833cde359de82fe588779e862ccb
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-05-24 22:21:17 +0300

    added brackets check

--- brackets_check.c
+++ brackets_check.c
@@ -11,44 +11,73 @@
 			4 braces
 */
 
-void cmnt_inpt(char c, char g);
+void cmnt_inpt(char c);
+void check_char(char c);
+void check_close(char c);
 char cmnt = 0;
 char qt = 0;
 char cls = 0;
+_Bool delay_cmnt;
 
 int main()
 {
-	char c,g;
+	char c;
 	
 	while((c = getchar()) != EOF)
 	{
-		if ((c == '/' || c == '*') && !qt)
+		check_char(c);
+		if (!cmnt)
+			check_close(c);
+		if (delay_cmnt)
 		{
-			g = getchar();
-			cmnt_inpt(c,g);
+			cmnt &= 1; //Turn off MLTCMT
+			delay_cmnt = 0;
 		}
-		else if (c == '\n')
-				cmnt &= 2; //turn off LNCMT
-		else if (c == '\"')
-				qt ^= 2;
-		else if (c == '\'')
-				qt ^= 1;
-		cls_ceck(c)
 	}
-	return 0;
+	printf("%d\n",cls);
+	return cls;
 }
 
-void cmnt_inpt(char c, char g)
+void cmnt_inpt(char c)
 {
-	if (c == '/' && g == '/')
-		cmnt |= 1; //Turn on LNCMT
-	else if (c =='/' && g == '*')
-		cmnt |= 2; //Turn on MLTCMT
-	else if (c == '*' && g == '/')
-		cmnt &= 1; //Turn off MLTCMT
-	else
-	{
-		if ((g == '(' || g == '[' or g == '{'))
-			cls_check(g)
+	char g = getchar();
+	if (c == '*' && g == '/')
+		delay_cmnt = 1;
+	else{
+		if (c == '/' && g == '/')
+			cmnt |= 1; //Turn on LNCMT
+		else if (c =='/' && g == '*')
+			cmnt |= 2; //Turn on MLTCMT
+		ungetc(g, stdin);
 	}
+}
+
+void check_char(char c)
+{
+	if (c == '\n')
+		cmnt &= 2; //turn off LNCMT
+	else if (c == '\"')
+		qt ^= 2;
+	else if (c == '\'')
+		qt ^= 1;
+	else if ((c == '/' || c == '*') && !qt)
+		{
+			cmnt_inpt(c);
+		}
+}
+
+void check_close(char c)
+{
+	if (c == '(')
+		cls |= 1;
+	else if (c == '[')
+		cls |= 2;
+	else if (c == '{')
+		cls |= 4;
+	else if (c == '}')
+		cls &= 4;
+	else if (c == ']')
+		cls &= 2;
+	else if (c == ')')
+		cls &= 1;
 }
