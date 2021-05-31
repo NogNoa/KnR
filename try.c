649b127b09792e1a2a1fc7199fc6f05e7cd01581
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-05-31 15:51:43 +0300

    I don't care that much

--- try.c
+++ try.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
-char * squeeze_multi(char s1[], char s2[]);
-char * squeeze(char s[], int c);
-char * strcat(char s[], char t[]);
-char * set(char s[]);
-int any(char s1[], char s2[]);
 unsigned getbits(unsigned x, int p, int n);
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 unsigned invert(unsigned x, int p, int n);
 unsigned rightrot(unsigned x, int n);
+void escape(char s[], char t[]);
+void descape(char s[], char t[]);
 
-void add_remove(int argc, char *argv[])
+void byte_foolish(void)
 {
-	int back;
-	if (argc > 2){
-		back = any(argv[1],argv[2]);
-		printf("%d\n",back);
-	}
+	printf("%x\n",rightrot(0xa6, 3));
+}
+
+void control_flow(char s[], char t[])
+{
+	descape(s, t);
 }
 
 int main(int argc, char *argv[])
 {
-	add_remove(argc,argv);
-	printf("%x\n",rightrot(0xa6, 3));
+	char back[0x100];
+
+	byte_foolish();
+	control_flow(back,argv[1]);
+	printf("%s\n",back);
+
 }
