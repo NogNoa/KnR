3d6fd539b07d07a9d4b39b22e8b6d489d00f9931
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-02 15:10:40 +0300

    got it out of my i\o

--- try.c
+++ try.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
-unsigned getbits(unsigned x, int p, int n);
-unsigned setbits(unsigned x, int p, int n, unsigned y);
-unsigned invert(unsigned x, int p, int n);
-unsigned rightrot(unsigned x, int n);
 void escape(char s[], char t[]);
 void descape(char s[], char t[]);
-
-void byte_foolish(void)
-{
-	printf("%x\n",rightrot(0xa6, 3));
-}
+char * file_stringise(char * filename);
 
 void control_flow(char s[], char t[])
 {
-	descape(s, t);
+	escape(s, t);
 }
 
 int main(int argc, char *argv[])
 {
-	char *call;
+	char * call;
 	char back[0x1000];
-	FILE *file = fopen("control_flow.c","r");
+	char filename[15]  = {'c','o','n','t','r','o','l','_','f','l','o','w','.','c'};
+	//FILE *file = fopen("./control_flow.c","r");
 
-	byte_foolish();	
-	fscanf(file, "%s", &call);
+	call = file_stringise(filename);
 	control_flow(back,call);
-	printf("%s\n",call);
-
+	printf("%s\n",back);
 }
