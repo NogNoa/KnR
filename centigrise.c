d816b04ffe923bab7be2833b01a57770569d422d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-07 18:55:44 +0300

    general centrigise

--- centigrise.c
+++ centigrise.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 double celsius(int fahr)
 {
 	return (5.0/9.0) * (fahr-32);
 }
 
-void main()
+int main(int argc, char *argv[])
 {
-	int fahr;
-	float lower = 0, upper = 300, step = 20;
+	float fahr, lower = 0, upper, step;
+	upper = (argc > 1) ? atof(argv[1]) : 300;
+	step = (argc > 2) ? atof(argv[2]) : 20;
 
     for (fahr = lower; fahr <= upper; fahr = fahr + step)
-        printf("%3df %6.1fc\n", fahr, celsius(fahr));;
+        printf("%3.ff %6.1fc\n", fahr, celsius(fahr));
+
+    return 0;
 }
