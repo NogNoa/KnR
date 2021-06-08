ec3b22b9638bd556e8f48a641c601599ebbbbacf
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-08 12:33:27 +0300

    this whole time
    Nobody told me I can assign strings like a normal person

--- try.c
+++ try.c
@@ -6,18 +6,19 @@ char * file_stringise(char * filename);
 
 void control_flow(char s[], char t[])
 {
-	escape(s, t);
+	descape(s, t);
+	escape(t, s);
 }
 
 int main(int argc, char *argv[])
 {
 	char * call;
 	char back[0x1000];
-	char filename[15]  = {'c','o','n','t','r','o','l','_','f','l','o','w','.','c'};
+	char filename[15]  = "control_flow.c";
 	//FILE *file = fopen("./control_flow.c","r");
 
 	call = file_stringise(filename);
 	control_flow(back,call);
-	printf("%s\n",back);
-	printf("The value of EOF is %x\n\n", EOF);
+	printf("%s\n",call);
+	printf("The value of EOF is %x\n", EOF);
 }
