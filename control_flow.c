e4d154b109875507eb735a4afee5256aa28ab217
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-09 14:04:34 +0300

    first try at expand

--- control_flow.c
+++ control_flow.c
@@ -1,3 +1,55 @@
+// Ritchie, D. and Kernighan, W. (1988) p54
+
+int KnR_binsearch(int x, int v[], int n)
+{ /* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+	int low, high, mid;
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (low+high)/2;
+		if (x < v[mid])
+			high = mid + 1;
+		else if (x > v[mid])
+			low = mid + 1;
+		else /* found match */
+			return mid;
+	}
+	return -1; /* no match */
+}
+
+int binsearch(int x, int v[], int n)
+{ /* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+	int low, high, mid;
+	low = 0;
+	high = n - 1;
+	while (low < high) {
+		mid = (low+high)/2;
+		if (x < v[mid])
+			high = mid + 1;
+		else
+			low = mid + 1;
+	}
+	if (low == high)
+		return low; //match
+	else 
+		return -1; // no match
+}
+/*
+assume   high = (low+high)/2 + 1; x = v[(low+high)/2] 
+happen	{high/2 = low/2 + 1
+	   	 high = low + 2}
+than    {(low < high)
+	       mid = high - 1
+	       x >= v[mid]
+	       low = mid + 1 = high
+	     (low >= high)
+	     ...}
+than 	 (x == v[mid]) iff (low == high)
+*/
+
+
+//Original
+
 void escape(char s[], char t[])
 {
 	char c;
