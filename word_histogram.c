20e057919b2f03a07ea50131176b8f90eebabfe4
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-16 22:58:10 +0300

    added vertical expose to word histogram

--- word_histogram.c
+++ word_histogram.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#define IN 1 /* inside a word */
-#define OUT 0 /* outside a word */
 #define MXWRD 0x20
 static inline _Bool unblank(char c)
 {
@@ -9,8 +7,10 @@ static inline _Bool unblank(char c)
 
 
 int measure(int hest[]);
-void expose(int hest[], int mxlen);
-void mkrow(char row[], int len);
+void expose_hor(int hest[], int mxlen);
+void mkrow_len(char row[], int len);
+void expose_ver(int hest[], int mxcnt);
+void mkrow_cnt(char row[], int hest[], int cnt, int mxlen);
 
 int main()
 {
@@ -18,10 +18,12 @@ int main()
 
 	mxlen = measure(hest);
     
-	expose(hest, mxlen);
+	expose_ver(hest, mxlen);
 
 	return 0;
 }
+
+int mxcnt = 0;
 
 int measure(int hest[])
 {
@@ -41,6 +43,8 @@ int measure(int hest[])
 			++hest[len];
 			if (len > mxlen)
                 mxlen = len;
+            if (hest[len] > mxcnt)
+            	mxcnt = hest[len];
 			len = 0;
 		}
 		prev=c;
@@ -48,22 +52,50 @@ int measure(int hest[])
     return mxlen;
 }
 
-void expose(int hest[], int mxlen)
+void expose_hor(int hest[], int mxlen)
 {
 	int i;
 	//char plus;
 	char row[0x300];
 
-	for (i=1; i<=(mxlen); ++i){
-		mkrow(row, hest[i]);
+	for (i=1; i<=mxlen; ++i){
+		mkrow_len(row, hest[i]);
 		printf("%2d  %s\n",i, row);
 	}
 }
 
-void mkrow(char row[], int len)
+void mkrow_len(char row[], int len)
 {
 	int j;
 	for (j=0; j<len; ++j)
 		row[j] ='|';
 	row[j] = '\0';
+}
+
+void expose_ver(int hest[], int mxlen)
+{
+	char row[0x300];
+
+	for (int i=mxcnt; i>0;--i)
+	{	mkrow_cnt(row, hest, i, mxlen);
+		printf("%2d %s\n",i, row);
+	}
+	printf("   ");
+	for (int i=1;i<=mxlen;++i)
+		printf("%3d",i);
+	putchar('\n');
+}
+
+void mkrow_cnt(char row[], int hest[], int cnt, int mxlen)
+{
+	int i,j=2;
+
+	for (i=1;i<=mxlen;i++,j+=3)
+		row[j-2]=row[j-1]=' ';
+	{	if (hest[i]>=cnt)
+			row[j]='|';
+		else
+			row[j]=' ';
+	}
+	row[j-2]='\0';
 }
