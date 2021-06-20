ab4805c541465f3b3abaa746eb623dac34b7e843
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-20 15:44:43 +0300

    unprintables first try

--- character_histogram.c
+++ character_histogram.c
@@ -5,9 +5,14 @@
 void main()
 {
 	char mxchar = (unsigned char) -1 >> 1;
-	char mnchar = ' ';
+	char mnchar = 0;
 	int i, j;
 	char hest[mxchar], c;
+
+	enum unprintables{NUL, SOH, STX, ETX, EOT, ENQ, ACK, BEL,
+					   BS,  HT,  LF,  VT,  FF,  CR,  SO,  SI
+					  DEL, DC1, DC2, DC3, DC4, NAK, SYN, ETB
+					  CAN,  EM, SUB, ESC,  FS,  GS,  RS,  US}
 	
 	for (i = 0; i<mxchar; ++i)
 		hest[i] = 0;
@@ -16,8 +21,17 @@ void main()
 			++hest[c];
 	}
 
-	for (i=mnchar; i<mxchar; ++i){
-		putchar(i);printf("  ");
+	for (i=0; hest[i]==0;++i)
+		mnchar=i;
+
+	for (i=0;i<' ';++i)
+		printf("%3s  ",unprintables[i]);
+		for (j=0; j<=hest[i]; ++j)
+			putchar('|');
+		putchar('\n');
+
+	for (;i<mxchar; ++i){
+		printf("  ");putchar(i);printf("  ");
 		for (j=0; j<=hest[i]; ++j)
 			putchar('|');
 		putchar('\n');
