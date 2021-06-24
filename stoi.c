88ab0794a42b57d66c9f28c52d654c34eb16d6a7
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-06-24 08:20:16 +0300

    now with both e and .

--- stoi.c
+++ stoi.c
@@ -185,8 +185,17 @@ double sci_atof(char s[])
 	sign = (s[i] == '-') ? -1 : 1;
 	if (s[i] == '+' || s[i] == '-')
 		i++;
+	
 	for (val = 0.0; isdigit(s[i]); i++)
 		val = 10.0 * val + (s[i] - '0');
+
+	if (s[i] == '.')
+	{	i++;
+		for (; isdigit(s[i]); i++) 
+		{	val = 10.0 * val + (s[i] - '0');
+			tens /= 10;
+		}
+	}
 
 	if (s[i] == 'e' || s[i] == 'E')
 			i++;
