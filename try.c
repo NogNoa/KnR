81a1b8a44cfd32c2c45184ace672d923be300c71
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-16 15:27:41 +0300

    check some constants

--- try.c
+++ try.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include <string.h>
+#include <ctype.h>
 #include "stoi.lb.c"
 void rcrs_itoa(int n, char s[]);
 
@@ -14,6 +14,9 @@ int main(int argc, char *argv[])
     rcrs_reverse(0,s);
     printf("%s\n",s);
     
+    printf("size_t:%lx signed int:%lx unsigned int:%lx \n int:%x char:%x\n",sizeof(size_t),sizeof(int signed),sizeof(int unsigned),(int)EOF,(char)EOF);
+
+
     return 0;
 
 }
