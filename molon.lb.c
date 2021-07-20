535a17994f5f0d96005eb0ddc540358ccb9de28d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-20 14:04:54 +0300

    continue try

--- molon.lb.c
+++ molon.lb.c
@@ -8,30 +8,25 @@ void ungetch(int);
 
 int getint(int *pn)
 {   /* getint: get next integer from input into *pn */
-	int c, g, sign;
-	while (isspace(g = getch())) /* skip white space */
+	signed char c, s=0, sign;
+	while (isspace(c = getch())) /* skip white space */
 		;
-	if (!isdigit(g) && g != EOF && g != '+' && g != '-') {
-		ungetch(g); /* it is not a number */
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c); /* it is not a number */
 		return 0;
 	}
-	sign = (g == '-') ? -1 : 1;
-	c = getch();
-	if (g == '+' || g == '-')
-	{	if (c == EOF)
-		{	ungetch(g);
-			return c; //EOF
-		}
-		else if (!isdigit(c))
-			{ungetch(c); ungetch(g);
-			return 0; //NaN
-			}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+	{	s=c;
+		c = getch();
 	}
 	for (*pn = 0; isdigit(c); c = getch())
 		*pn = 10 * *pn + (c - '0');
 	*pn *= sign;
 	if (c != EOF)
 		ungetch(c);
+	if (s && !*pn)
+		ungetch(s);
 	return c;
 }
 
--- try.c
+++ try.c
@@ -6,11 +6,11 @@
 
 int main(int argc, char *argv[])
 {
-    int n[0x100]={0};
+    int n[0x100]={0}, r;
     double m[0x100]={0};
 
-    getint(n);
-    printf("%d\n",*n);
+    r=getint(n);
+    printf("%d %x\n",*n,r);
     getfloat(m);
     printf("%f\n",*m);
 
