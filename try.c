0d0135bf3bebe855e1765363963a5b259b270f1e
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-24 17:56:11 +0300

    Revert "Now it works"
    This reverts commit 873e2c5dd5b76137c63312074b63d57527600b22.

--- try.c
+++ try.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "add_remove-string.lb.c"
+#include "KnR_getline.lb.c"
+#include "stoi.lb.c"
+#include "control_flow.lb.c"
+
 
 int main(int argc, char *argv[])
 {
-    char s[]="molon ";
-    char sh[]="labe! ";
-    printf("%s\n",alt_strcat(s,sh));
-    printf("%d\n",strend("vga\0brr","ga"));
-    char *t = malloc(1024);
-    t=alt_strncpy(t, "atzmi", 6);
-    printf("%s\n",alt_strncpy(t, "ani", 3));
-    t = alt_strncat(t," veatzmi",7);
-    printf("%s\n",t);
-    printf("%d\n",alt_strncmp("vfb","vga",2));
+    char s[1024];
+    int i=ptr_KnR_getline(s,128);
+    printf("%s %d\n",s,i);
+    itob(i,s,18);
+    printf("%s %d\n",s,atoi("128"));
 
     return 0;
 
