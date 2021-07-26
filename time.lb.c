39fee97f53ec13a062990fa6b7f3f3fb8c9eea06
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-27 01:39:27 +0300

    Booleans baby
    (actually ints)

--- time.lb.c
+++ time.lb.c
@@ -1,8 +1,7 @@
 // Ritchie, D. and Kernighan, W. (1988) p99
 
-static char daytab[2][13] = {
+static char daytab[13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
 int day_of_year(int year, int month, int day)
@@ -10,7 +9,8 @@ int day_of_year(int year, int month, int day)
 	int i, leap;
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 	for (i = 1; i < month; i++)
-		day += daytab[leap][i];
+		day += daytab[i];
+	day += (leap && 2 < month)
 	return day;
 }
 
@@ -18,8 +18,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 { /* month_day: set month, day from day of year */
 	int i, leap;
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-	for (i = 1; yearday > daytab[leap][i]; i++)
-		yearday -= daytab[leap][i];
+	yearday -= (leap && yearday >= 60)
+	for (i = 1; yearday > daytab[i]; i++)
+		yearday -= daytab[i];
 	*pmonth = i;
 	*pday = yearday;
 }
