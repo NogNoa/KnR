dea93fb3db41febe94da79597c32bb1c42164247
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-07-29 19:08:50 +0300

    error check

--- time.lb.c
+++ time.lb.c
@@ -4,9 +4,11 @@ static char daytab[13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 };
 
-int day_of_year(int year, int month, int day)
+int day_of_year(int year,unsigned int month,unsigned int day)
 {  /* day_of_year: set day of year from month & day */
 	int i, leap;
+	if (!day || !month || year < 1582 || day > 31 || month > 12)
+		return -1;
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 	for (i = 1; i < month; i++)
 		day += daytab[i];
@@ -17,6 +19,8 @@ int day_of_year(int year, int month, int day)
 void month_day(int year, int yearday, int *pmonth, int *pday)
 { /* month_day: set month, day from day of year */
 	int i, leap;
+	if (year < 1582 || yearday < 1 || yearday > 366)
+		return -1;
 	leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
 	yearday -= (leap && yearday >= 60)
 	for (i = 1; yearday > daytab[i]; i++)
