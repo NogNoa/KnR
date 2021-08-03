8ad37e3dd518a77931d69d06b1584f05489d4650
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-03 12:13:12 +0300

    general detab

--- detab.c
+++ detab.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-
-int main(int argc, char *argv)
-{
-	if (argc < 2)
-		/* tab-stops of 4-spaces each. */
-		back = detab_reg(4)
-	else 
-		back = detab(argv)
-	return back
-}
+#include <stdlib.h>
 
 int detab_reg(int gap)
 {
@@ -32,7 +23,49 @@ int detab_reg(int gap)
 	return i;
 }
 
-int detab(char *argv)
+int detab(int len, int *stops)
 {
-	return 0;
+	int i=0;
+	char c;
+	
+	while ((c = getchar()) != EOF){
+		if (c == '\t')
+		{	_Bool cnt=1;
+			while (cnt)
+			{	putchar(' ');
+				 ++i;
+				for (int*p=stops;p<stops+len;++p)
+					if (*p==i)
+						cnt=0;
+					else if
+			}
+		}
+		else if (c == '\n'){
+			putchar(c);
+			i = 0;
+		}
+		else{
+			putchar(c);
+			++i;
+		}
+	}
+	return i;	
+
+}
+
+int main(int argc, char *argv)
+{
+	int back;
+
+	if (argc < 2)
+		/* tab-stops of 4-spaces each. */
+		back = detab_reg(4);
+	else 
+	{	int stops[argc-1];
+		for (int i=1;i<argc;i++)
+		{	stops[i-1] = atoi(argv+i);
+		}
+		back = detab(argc, stops);
+	}
+	return back;
 }
