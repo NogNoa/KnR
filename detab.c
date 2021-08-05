589120ba9a1bf260fd371c9d034f0b1842b05b6e
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-05 14:47:49 +0300

    getting a unified input ready for detab and detab_reg

--- detab.c
+++ detab.c
@@ -78,36 +78,44 @@ int detab(int len, int *stops)
 	return i;
 }
 
+struct tabbin 
+{	int n,m;
+	int *stops;
+};
+
+struct tabbin arg_prcs(int argc, char *argv[])
+{
+	struct tabbin taby;
+	taby.stops = malloc(argc+1);
+	taby.n=0,taby.m=0;
+	taby.stops[0]=0; taby.stops[argc+1]=MXLIN;
+	for (int i=1;i<argc;i++)
+	{	if (*argv[i] == '+')
+			taby.n=atoi(argv[i]+1);
+		else if (*argv[i] == '-')
+			taby.m=atoi(argv[i]+1);
+		else
+			taby.stops[i] = atoi(argv[i]);
+	}
+	qsort(taby.stops,argc+1,sizeof(int),cmp);
+	if (0< taby.stops[0] || taby.stops[argc] > MXLIN)
+		fprintf(stderr,"ERROR: Please enter tabstops between 0 and %d\n",MXLIN);
+	return taby;
+
+}
+
 int main(int argc, char *argv[])
 {
 	int back;
+	struct tabbin taby;
 	//stdin = fopen("detab.c", "r");
 	
 	if (argc < 2)
 		/* tab-stops of 4-spaces each. */
 		back = detab_reg(0,4);
 	else 
-	{	int n=0,m=0;
-		for (int i=1;i<argc;i++)
-		{	if (*argv[i] == '+')
-				n=atoi(argv[i]+1);
-			else if (*argv[i] == '-')
-				m=atoi(argv[i]+1);
-		}
-		if (n)	
-			back = detab_reg(m,n);
-		else 
-		{	int stops[argc+1];
-			for (int i=1;i<argc;i++)
-			{	stops[i] = atoi(argv[i]);
-			}
-			stops[0]=0; stops[argc+1]=MXLIN;
-			//KnR_qsort(stops,0,argc-2);
-			qsort(stops,argc+1,sizeof(int),cmp);
-			if (0< stops[0] || stops[argc] > MXLIN)
-				fprintf(stderr,"ERROR: Please enter tabstops between 0 and %d\n",MXLIN);
-			back = detab(argc+1, stops);
-		}
+	{	taby = arg_prcs(argc, argv);
+		back = detab(argc+1,taby.stops);
 	}
 	return back;
 }
