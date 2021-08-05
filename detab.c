bef46afd6191410f306b27022528584e9b184c5b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-05 17:48:34 +0300

    make arg_prcs available for entab
    also aestetic change from low to high bound
    an additional little math change

--- detab.c
+++ detab.c
@@ -3,12 +3,7 @@
 #include <ctype.h>
 #define MXLIN 72 /* max line length*/
 
-void KnR_qsort(int v[], int left, int right);
-
-int cmp(const void *a, const void *b)
-{
-	return ( *(int*)a - *(int*)b );
-}
+struct tabbin arg_prcs(int argc, char *argv[]);
 
 int *fndcrsr(int i, int *stops, int len) //find cursor
 {
@@ -16,7 +11,7 @@ int *fndcrsr(int i, int *stops, int len) //find cursor
 	for (p=stops;p<stops+len;++p)
 		if (*p <= i && i < *(p+1))
 			break;
-	return p;	
+	return p+1;	
 }
 
 
@@ -31,12 +26,12 @@ int detab(int start, int gap, int *stops, int len)
 				i=0;
 			}
 		else if (c == '\t')
-		{	int *lwbd; //low boundary
-			lwbd = fndcrsr(i,stops,len);
-			int gpstp = (i/gap+1)*gap; //gap stop
+		{	int *hibd; //high boundary
+			hibd = fndcrsr(i,stops,len);
+			int gpstp = start + ((i-start)/gap+1)*gap; //gap stop
 			if (i<start) while (i++ < start)
 				putchar(' ');
-			else while(i++ < *(lwbd+1) && i < gpstp) 
+			else while(i++ < *(hibd) && i < gpstp) 
 				putchar(' ');
 			if (i>=MXLIN)
 			{	putchar('\n');
@@ -59,27 +54,6 @@ struct tabbin
 {	int n,m;
 	int *stops;
 };
-
-struct tabbin arg_prcs(int argc, char *argv[])
-{
-	struct tabbin taby;
-	taby.stops = malloc(argc+1);
-	taby.n=0,taby.m=0;
-	taby.stops[0]=0; taby.stops[argc+1]=MXLIN;
-	for (int i=1;i<argc;i++)
-	{	if (*argv[i] == '+')
-			taby.n=atoi(argv[i]+1);
-		else if (*argv[i] == '-')
-			taby.m=atoi(argv[i]+1);
-		else
-			taby.stops[i] = atoi(argv[i]);
-	}
-	qsort(taby.stops,argc+1,sizeof(int),cmp);
-	if (0< taby.stops[0] || taby.stops[argc] > MXLIN)
-		fprintf(stderr,"ERROR: Please enter tabstops between 0 and %d\n",MXLIN);
-	return taby;
-
-}
 
 int main(int argc, char *argv[])
 {
---
+++ tabin.lb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MXLIN 72 /* max line length*/
+
+int cmp(const void *a, const void *b)
+{
+	return ( *(int*)a - *(int*)b );
+}
+
+struct tabbin 
+{	int n,m;
+	int *stops;
+};
+
+struct tabbin arg_prcs(int argc, char *argv[])
+{
+	struct tabbin taby;
+	taby.stops = malloc(argc+1);
+	taby.n=0,taby.m=0;
+	taby.stops[0]=0; taby.stops[argc+1]=MXLIN;
+	for (int i=1;i<argc;i++)
+	{	if (*argv[i] == '+')
+			taby.n=atoi(argv[i]+1);
+		else if (*argv[i] == '-')
+			taby.m=atoi(argv[i]+1);
+		else
+			taby.stops[i] = atoi(argv[i]);
+	}
+	qsort(taby.stops,argc+1,sizeof(int),cmp);
+	if (0< taby.stops[0] || taby.stops[argc] > MXLIN)
+		fprintf(stderr,"ERROR: Please enter tabstops between 0 and %d\n",MXLIN);
+	return taby;
+}
