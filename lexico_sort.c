f7d320ba85f1c09607c373515bba907f7aa715ce
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-08 22:03:18 +0300

    update lexico_sort
    to p106

--- lexico_sort.c
+++ lexico_sort.c
@@ -1,6 +1,6 @@
 // Ritchie, D. and Kernighan, W. (1988) p97
+// Ritchie, D. and Kernighan, W. (1988) p106
 
-#include <string.h>
 #include "KnR_getline.h"
 #define MAXLINES 5120 /* max #lines to be sorted */
 
@@ -9,14 +9,19 @@ char *lineptr[MAXLINES]; /* pointers to text lines */
 int readlines(char *lineptr[], int nlines, char *snglptr);
 void writelines(char *lineptr[], int nlines);
 void qsort(char *lineptr[], int left, int right);
+int numcmp(char *s1, char *s2);
 #define MAXLEN 1024 /* max length of any input line */
 
-int main()
+int main(int argc, char *argv[])
 {	/* sort input lines */
 	int nlines; /* number of input lines re'd */
 	char buffer[MAXLEN];
+	int numeric = 0; /* 1 if numeric sort */
+	if (argc > 1 && strcmp(argv[1], "-n") == 0)
+		numeric = 1;
 	if ((nlines = readlines(lineptr, MAXLINES,buffer)) >= 0) {
-		qsort(lineptr, 0, nlines-1);
+		qsort((void**) lineptr, 0, nlines-1,
+		(int (*)(void*,void*))(numeric ? numcmp : strcmp));
 		writelines(lineptr, nlines);
 		return 0;
 	} else {
@@ -25,6 +30,19 @@ int main()
 	}
 }
 
+int numcmp(char *s1, char *s2)
+{	/* numcmp: compare s1 and s2 numerically */
+	double v1, v2;
+
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	else
+		return 0;
+}
 
 #ifndef alloc
 	#include "molon.lb.c"
@@ -65,19 +83,20 @@ void swap(char *v[], int i, int j)
 	v[j] = temp;
 }
 
-void qsort(char *v[], int left, int right)
+void qsort(char *v[], int left, int right,
+	int (*cmp)(void *, void *))
 { /* qsort: sort v[left]...v[right] into increasing order */
 	int i, last;
 	void swap(char *v[], int i, int j);
 
 	if (left >= right) /* do nothing if array contains */
-		return; /* fewer than two elements */
+		return; 	  /* fewer than two elements      */
 	swap(v, left, (left + right)/2);
 	last = left;
 	for (i = left+1; i <= right; i++)
-		if (strcmp(v[i], v[left]) < 0)
+		if (*(cmp)(v[i], v[left]) < 0)
 			swap(v, ++last, i);
 	swap(v, left, last);
-	qsort(v, left, last-1);
-	qsort(v, last+1, right);
+	qsort(v, left, last-1, cmp);
+	qsort(v, last+1, right, cmp);
 }
