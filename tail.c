f11940adb294f2438f3741dd1759a62b2e737ceb
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-08 18:42:35 +0300

    ceiling for tail

--- tail.c
+++ tail.c
@@ -3,10 +3,15 @@
 int main(int argc, char *argv[])
 {
 	int tail=10, nline;
+
+	//stdin = fopen("tail.c", "r");
 	
 	nline = linearise();
 	if (argc > 1 && argv[1][0] == '-')
 		tail=atoi(argv[1]+1);
+	tail = (tail < nline) ? tail : nline;
 	for (int i;(i=nline-tail)<nline;tail--)
 		printf("%s",lini[i]);
 }
+
+/* todo?: hard to identify sementation fault on printing line 4:"{\n"*/
