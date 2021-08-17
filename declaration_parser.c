2db9e9b7df84929c55c392320d69d6c505f6fbf5
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-17 17:42:52 +0300

    printing

--- declaration_parser.c
+++ declaration_parser.c
@@ -21,7 +21,7 @@ int main()
 		out[0] = '\0';
 		dcl(); /* parse rest of line */
 		if (tokentype != '\n')
-			printf("syntax error\n");
+			printf("syntax error %d\n",tokentype);
 		printf("%s: %s %s\n", name, out, datatype);
 	}
 	return 0;
--- undcl.c
+++ undcl.c
@@ -7,16 +7,17 @@ int main()
 	while (gettoken() != EOF) 
 	{	strcpy(out, token);
 		while ((type = gettoken()) != '\n')
-		if (type == PARENS || type == BRACKETS)
-			strcat(out, token);
-		else if (type == '*') 
-		{	sprintf(temp, "(*%s)", out);
-			strcpy(out, temp);
-		} else if (type == NAME) 
-		{	sprintf(temp, "%s %s", token, out);
-			strcpy(out, temp);
-		} else
-			printf("invalid input at %s\n", token);
+			if (type == PARENS || type == BRACKETS)
+				strcat(out, token);
+			else if (type == '*') 
+			{	sprintf(temp, "(*%s)", out);
+				strcpy(out, temp);
+			} else if (type == NAME) 
+			{	sprintf(temp, "%s %s", token, out);
+				strcpy(out, temp);
+			} else
+				printf("invalid input at %s\n", token);
+		printf("%s\n",out);
 	}
 	return 0;
 }
