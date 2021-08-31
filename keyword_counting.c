52e3cab560ac38cf977fce513f66b3852bcbe9a9
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-08-31 17:36:56 +0300

    adding the needed objects

--- keyword_counting.c
+++ keyword_counting.c
@@ -1,14 +1,54 @@
 // Ritchie, D. and Kernighan, W. (1988) p119
 
-include <stdio.h>
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #define MAXWORD 100
 
-int getword(char *, int);
+struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	"auto", 0,
+	"break", 0,
+	"case", 0,
+	"char", 0,
+	"const", 0,
+	"continue", 0,
+	"default", 0,
+	"double", 0,
+	"int", 0,
+	"struct", 0,
+	"else", 0,
+	"long", 0,
+	"switch", 0,
+	"enum", 0,
+	"register", 0,
+	"typedef", 0,
+	"extern", 0,
+	"return", 0,
+	"union", 0,
+	"float", 0,
+	"short", 0,
+	"for", 0,
+	"signed", 0,
+	"goto", 0,
+	"sizeof", 0,
+	"do", 0,
+	"if", 0,
+	"static", 0,
+	"unsigned", 0,
+	"void", 0,
+	"volatile", 0,
+	"while", 0
+};
+
+static const int NKEYS = sizeof(keytab)/sizeof(struct key);
+
+int getword(char *, int); /* in molon.lb.c */
 int binsearch(char *, struct key *, int);
 
-main()
+int main()
 { /* count C keywords */
 	int n;
 	char word[MAXWORD];
