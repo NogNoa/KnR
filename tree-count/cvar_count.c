fefdc9b8f480b053ee442c6978b5ebcb1a26211a
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-15 11:53:53 +0300

    another idea

--- tree-count/cvar_count.c
+++ tree-count/cvar_count.c
@@ -1,3 +1,16 @@
+int main()
+{ /* word frequency count */
+	struct tnode *root;
+	char word[MAXWORD];
+	
+	root = NULL;
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			root = addtree(root, word);
+	treeprint(root);
+	return 0;
+}
+
 #include <string.h>
 
 static char* keywords[] = {
--- tree-count/tree_count.h
+++ tree-count/tree_count.h
@@ -8,3 +8,9 @@ struct tnode { /* the tree node: */
 	struct tnode *left; /* left child */
 	struct tnode *right; /* right child */
 };
+
+struct htnode {// node of a wordhead tree
+	char *head; // the string that make the wordhead
+	int count; 
+	char tnode *headroot; // the first word in the tree that has this head
+}
