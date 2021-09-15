1f07761157fb980128fda5003d842d856e3ae84b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-15 13:49:35 +0300

    unite detab and entab
     into one file with a flag

--- entab.c
+++ entab.c
@@ -35,21 +35,63 @@ int entab(int strt, int gap, int *stops, int len)
 	return i;
 }
 
+#include <ctype.h>
+
+int detab(int start, int gap, int *stops, int len)
+{
+	int i=0;
+	char c;
+	
+	while ((c = getchar()) != EOF)
+	{	if (isspace(c) && i >= MXLIN)
+			{	putchar('\n');
+				i=0;
+			}
+		else if (c == '\t')
+		{	int nxtstp = fndstp(start, gap, stops, len, i); //nxtstp
+			for(;i < nxtstp;i++) 
+				putchar(' ');
+			if (i>=MXLIN)
+			{	putchar('\n');
+				i = 0;
+			}
+		}
+		else if (c == '\n')
+		{	putchar(c);
+			i = 0;
+		}
+		else
+		{	putchar(c);
+			++i;
+		}
+	}
+	return i;
+}
+
 int main(int argc, char *argv[])
 {
 	int back;
 	struct tabbin taby;
+	_Bool bentab = 1;
+	int (*funtab)(int,int,int*,int);
 
-	//stdin = fopen("a.txt","r");
+	if (bentab)
+		funtab=*entab;
+	else
+		funtab=*detab;
+
+	//stdin = fopen("entab.c","r");
 
 	if (argc < 2)
 	{	/* tab-stops of 4-spaces each. */
 		int stops[2] = {0,MXLIN};
-		back = entab(0, 4, stops, 2);
+		back = funtab(0, 4, stops, 2);
 	}
 	else
 	{	taby = arg_prcs(argc, argv);
-		back = entab(taby.m, taby.n, taby.stops, argc+1);
+		back = funtab(taby.m, taby.n, taby.stops, argc+1);
 	}
 	return back;
 }
+
+/* todo: last tab stays spaces */
