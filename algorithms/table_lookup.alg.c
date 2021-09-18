5cb64a2530268b6c55cf51fea569a0fda1f8beb3
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-18 10:52:40 +0300

    Create table_lookup.alg.c

---
+++ algorithms/table_lookup.alg.c
@@ -0,0 +1,29 @@
+p1 = install("IN", "1")
+	np := lookup("IN")
+		np := hashtab[hash("IN")]
+			hashval := 'I' + 31 * 0 = 'I'
+			hashval = 'N' + 31 * 'I' = x4e + x1f * x49 = x4e + x8d7 = x925
+			return x925 % x65 = x925 - x913 = x12 = 18
+		np = hashtab[18] = NULL
+		return NULL
+	np = NULL
+	np := (struct nlist *) malloc()
+	np != NULL 
+	np->name := "IN" != NULL
+	hashval := hash("IN") = 18
+	np->next := hastab[18]
+	hastab[18] := np
+	np->defn = "1" != NULL
+	return np = {.next=(hastab[18] = np), .name="IN", .defn="1"}
+
+s2 != "IN" hash(s2)=18
+
+p2 = install(s2, "2")
+	np := lookup(s2)
+		np := hashtab[18] = p1 != NULL
+		strcmp(s2, p1->name="IN") != 0
+		np := p1->next = hashtab[18] = p1 != NULL
+		ad infinitum
+		suppose ("np->next = hashtab[hashval]") is deleted
+		np := p1->next = NULL
+		doesn't help
