4f16f926c651f8af21d64c14a8702e898f466d32
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-18 13:12:33 +0300

    Update table_lookup.alg.c

--- algorithms/table_lookup.alg.c
+++ algorithms/table_lookup.alg.c
@@ -1,4 +1,4 @@
-p1 = install("IN", "1")
+p1 := install("IN", "1")
 	np := lookup("IN")
 		np := hashtab[hash("IN")]
 			hashval := 'I' + 31 * 0 = 'I'
@@ -11,19 +11,37 @@ p1 = install("IN", "1")
 	np != NULL 
 	np->name := "IN" != NULL
 	hashval := hash("IN") = 18
-	np->next := hastab[18]
+	np->next := hastab[18] = NULL
 	hastab[18] := np
 	np->defn = "1" != NULL
-	return np = {.next=(hastab[18] = np), .name="IN", .defn="1"}
+	return np = {.next=NULL, .name="IN", .defn="1"}
 
 s2 != "IN" hash(s2)=18
 
-p2 = install(s2, "2")
+p2 := install(s2, "2")
 	np := lookup(s2)
 		np := hashtab[18] = p1 != NULL
 		strcmp(s2, p1->name="IN") != 0
-		np := p1->next = hashtab[18] = p1 != NULL
-		ad infinitum
-		suppose ("np->next = hashtab[hashval]") is deleted
 		np := p1->next = NULL
-		doesn't help
+		return NULL
+	np = NULL
+	np := (struct nlist *) malloc() != NULL
+	hashval := hash(s2) = 18
+	np->next := hashtab[18] = p1
+	hastab[18] := np
+	np->defn := "2" != NULL
+	return np = {.next=p1, .name=s2, .defn="2"}
+
+p3 := install("IN", 3)
+	np := lookup("IN")
+		np := hashtab[hash("IN")=18] = p2 != NULL
+		strcmp("IN", p2->name=s2) != 0
+		np := p2->next = p1 != NULL
+		strcmp("IN", p1->name="IN") = 0
+		return np = p1
+	np != NULL
+	free((np=p1)->defn)
+	(np=p1)->defn := "3" != NULL
+	return np = {.next=NULL, .name="IN", .defn="3"}
+p1 = p3 
+
