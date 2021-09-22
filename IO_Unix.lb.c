4bbc6e350423c765c9d7a8687855dc67adb8641b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-22 22:48:15 +0300

    expend to include all conversion specification types

--- IO_Unix.lb.c
+++ IO_Unix.lb.c
@@ -1,12 +1,19 @@
 // Ritchie, D. and Kernighan, W. (1988) p128
 
-#include <stdarg.h>
+#ifndef va_list
+	#include <stdarg.h>
+#endif
+#ifndef putchar
+	#include <stdio.h>
+#endif
+
 
 void minprintf(char *fmt, ...)
 {  /* minprintf: minimal printf with variable argument list */
 	va_list ap; /* points to each unnamed arg in turn */
-	char *p, *sval;
+	char *p, *sval, minfmt[3];
 	int ival;
+	unsigned uval;
 	double dval;
 	
 	va_start(ap, fmt); /* make ap point to 1st unnamed arg */
@@ -16,22 +23,39 @@ void minprintf(char *fmt, ...)
 			continue;
 		}
 		
-		switch (*++p) 
+		switch (*++p)
 		{case 'd':
+		case 'i':
+		case 'x':
+		case 'X':
+		case 'o':
 			ival = va_arg(ap, int);
-			printf("%d", ival);
-		break;
+			sprintf(minfmt,"%%%c",*p);
+			printf(minfmt, ival);
+			break;
+		case 'u':
+			uval = va_arg(ap, unsigned);
+			printf("%u", uval);
+			break;
 		case 'f':
+		case 'e':
+		case 'E':
+		case 'g':
+		case 'G':
 			dval = va_arg(ap, double);
-			printf("%f", dval);
-		break;
+			sprintf(minfmt,"%%%c",*p);
+			printf(minfmt, dval);
+			break;
+		case 'p':
+			printf("%p", va_arg(ap, void *));
+			break;
 		case 's':
 			for (sval = va_arg(ap, char *); *sval; sval++)
-			putchar(*sval);
-		break;
+				putchar(*sval);
+			break;
 		default:
 			putchar(*p);
-		break;
+			break;
 		}
 	}
 	va_end(ap); /* clean up when done */
