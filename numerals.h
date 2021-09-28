90d4b794bccd203e12bb54c25bc5a3a796f7b0e3
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-28 12:46:38 +0300

    more functions

--- numerals.h
+++ numerals.h
@@ -8,17 +8,34 @@ int binarise_single(bcd call)
 	return (signed) (call.eight) ? (call.rest > 1 ? 10 : 8 + call.rest) : call.rest;
 }
 
+//bcd arrs are little endian, as all things should be.
+
 int binarise(bcd call[], int len)
 {
-	;//if (!call)
+	if (!call)
+		return 0;
+	else
+		return binarise_single(*call) + 10*binarise(call+1);
 }
 
 void reveal(bcd call)
 {
-	printf("%d",binarise_single(call));
+	printf("%d",binarise(call));
 }
 
-bcd decimise(int)
+bcd decimise_single(int call)
 {
-	;
+	return {.eight = (call > 7), .rest = call % 8};
+}
+
+bcd* decimise(int call, bcd back[])
+{
+	if (!call)
+		*back = {0,0};
+	else
+	{	*back = decimise_single(call % 10); 
+		decimise(call / 10, back_ptr+1);
+	}	
+
+	return back;
 }
