73fc346293cf21663e69ca696d611de31debdc91
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-28 14:38:18 +0300

    modularise try

--- find.c
+++ find.c
@@ -5,42 +5,49 @@
 
 // Ritchie, D.M. and Kernighan, B.W. (1988) p104
 
-int find(int argc, char *argv[])
-{ /* find: print lines that match pattern from 1st arg */
-	char line[MAXLINE];
-	long lineno = 0;
-	int c, except = 0, number = 0, found = 0;
+int find(int argc, char *argv[], struct stt state);
+
+int main(int argc, char *argv[])
+{	char c; 
+	struct stt
+	{	int found;
+		_Bool except, number;
+	} state = {0,0,0};
+
 	
 	while (--argc > 0 && (*++argv)[0] == '-')
 		while ((c = *++argv[0]))
 		switch (c) 
 		{	case 'x':
-				except = 1;
+				state.except = 1;
 			break;
 			case 'n':
-				number = 1;
+				state.number = 1;
 			break;
 			default:
 				printf("find: illegal option %c\n", c);
 				argc = 0;
-				found = -1;
+				state.found = -1;
 			break;
 		}
+	return find(argc, argv, state);
+}
+
+int find(int argc, char *argv[], struct stt state)
+{ /* find: print lines that match pattern from 1st arg */
+	char line[MAXLINE];
+	long lineno = 0;
+
 	if (argc != 1)
 		printf("Usage: find -x -n pattern\n");
 	else while (fgets(line, MAXLINE, stdin) > 0)
 	{	lineno++;
-		if ((strstr(line, *argv) != NULL) != except) 
-		{	if (number)
+		if ((strstr(line, *argv) != NULL) != state.except) 
+		{	if (state.number)
 				printf("%ld:", lineno);
 			printf("%s", line);
-			found++;
+			state.found++;
 		}
 	}
 	return found;
-}
-
-
-int main(int argc, char *argv[])
-{	return find(argc, argv);
 }
