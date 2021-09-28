f775feb9dab80c46e08f574aa97e7d1af0a13306
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-28 14:07:11 +0300

    cleanup

--- find.c
+++ find.c
@@ -1,43 +1,9 @@
 #include <string.h>
 #include "KnR_getline.lb.c"
-#include "add_remove-string.lb.c"
-
-// Ritchie, D.M. and Kernighan, B.W. (1988) p63
-
-char pattern[] = "ould"; /* pattern to search for */
-
-int find40()
-{	/* find all lines matching pattern */
-	char line[MAXLINE];
-	int found = 0;
-	
-	while (KnR_getline(line, MAXLINE) > 0)
-		if (strindex_1st(line, pattern) >= 0) 
-		{	printf("%s", line);
-			found++;
-		}
-	return found;
-}
 
 // Ritchie, D.M. and Kernighan, B.W. (1988) p104
 
-int find54(int argc, char *argv[])
-{ /* find: print lines that match pattern from 1st arg */
-	char line[MAXLINE];
-	int found = 0;
-	
-	if (argc != 2)
-		printf("Usage:find pattern\n");
-	else while (KnR_getline(line, MAXLINE) > 0)
-		if (strstr(line, argv[1]) != NULL)
-		{	printf("%s", line);
-			found++;
-		}
-	return found;
-}
-
-
-int find55(int argc, char *argv[])
+int find(int argc, char *argv[])
 { /* find: print lines that match pattern from 1st arg */
 	char line[MAXLINE];
 	long lineno = 0;
@@ -74,5 +40,5 @@ int find55(int argc, char *argv[])
 
 
 int main(int argc, char *argv[])
-{	return find55(argc, argv);
+{	return find(argc, argv);
 }
