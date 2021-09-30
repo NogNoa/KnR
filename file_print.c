f7270cddc16349be684746f344a4cc4ca63c266b
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-09-30 19:48:50 +0300

    completed file_print

--- file_print.c
+++ file_print.c
@@ -6,11 +6,13 @@
 char ** strarr_allocate(int nom_cnt, char **nomi, char **codii);
 FILE * file_switch(FILE *codex, char** codii);
 
+//internal
+int file_print(FILE *codex);
+
 int main(int argc, char *argv[])
 {
 	char **codii;
 	FILE *codex;
-	int count_codii = 0;
 
 	if (argc > 1)
 		codii = strarr_allocate(--argc, ++argv, codii);
@@ -20,16 +22,23 @@ int main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
-int file_print(File *codex)
+int file_print(FILE *codex)
 {
 	static int page_num = 1;
-	       int line_num = 1;
+	       int line_num    ;
 	size_t maxline = 0200;
 	char *line = (char *) malloc(maxline);
+	_Bool nextpage=1;
 
-	for (getline(&line, &maxline, codex) > 0)
-	{	while(line_num<PAGELINI)
+	while (nextpage)
+	{	for(line_num=1;line_num<PAGELINI;line_num++)
+		{	if (!nextpage)
+				line="\n";
+			else
+		 		nextpage =getline(&line, &maxline, codex);
 			printf("%s",line);
-		printf("%40d",page_num++)
-
+		}
+		printf("%40d",page_num++);
+	}
+	return page_num;
 }
