950dfd4a10c09d11f22d59c85d5d57d002387a05
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-10-03 16:10:35 +0300

    stdio.lb.c

--- stdio.lb.h
+++ stdio.lb.h
@@ -1,7 +1,10 @@
+// Ritchie, D.M. and Kernighan, B.W. (1988) p156
+
 #define NULL 0
 #define EOF (-1)
 #define BUFSIZ 1024
 #define OPEN_MAX 20 /* max #files open at once */
+
 typedef struct _iobuf {
 	int cnt; /* characters left */
 	char *ptr; /* next character position */
@@ -9,11 +12,13 @@ typedef struct _iobuf {
 	int flag; /* mode of file access */
 	int fd; /* file descriptor */
 } FILE;
-157
+
 extern FILE _iob[OPEN_MAX];
+
 #define stdin (&_iob[0])
 #define stdout (&_iob[1])
 #define stderr (&_iob[2])
+
 enum _flags {
 	_READ = 01, /* file open for reading */
 	_WRITE = 02, /* file open for writing */
@@ -21,14 +26,18 @@ enum _flags {
 	_EOF = 010, /* EOF has occurred on this file */
 	_ERR = 020 /* error occurred on this file */
 };
+
 int _fillbuf(FILE *);
 int _flushbuf(int, FILE *);
+
 #define feof(p) ((p)->flag & _EOF) != 0
 #define ferror(p) ((p)->flag & _ERR) != 0
 #define fileno(p) ((p)->fd)
 #define getc(p) (--(p)->cnt >= 0 \
 ? (unsigned char) *(p)->ptr++ : _fillbuf(p))
+
 #define putc(x,p) (--(p)->cnt >= 0 \
 ? *(p)->ptr++ = (x) : _flushbuf((x),p))
+
 #define getchar() getc(stdin)
 #define putchqr(x) putc((x), stdout)
