08bb0b41e1c3dbd085de6d96019a3bd2c4b094ba
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-10-04 22:24:19 +0300

    complete implement the fld varient

--- stdio.lb.h
+++ stdio.lb.h
@@ -36,12 +36,15 @@ enum _flags {
 
 int _fillbuf(FILE *);
 int _flushbuf(int, FILE *);
+int _fillbuf_fld(FILE *);
 
 #define feof(p) ((p)->flag & _EOF) != 0
 #define ferror(p) ((p)->flag & _ERR) != 0
 #define fileno(p) ((p)->fd)
 #define getc(p) (--(p)->cnt >= 0 \
 ? (unsigned char) *(p)->ptr++ : _fillbuf(p))
+#define getc_fld(p) (--(p)->cnt >= 0 \
+? (unsigned char) *(p)->ptr++ : _fillbuf_fld(p))
 
 #define putc(x,p) (--(p)->cnt >= 0 \
 ? *(p)->ptr++ = (x) : _flushbuf((x),p))
--- try.c
+++ try.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include "istable.lb.h"
+#include "stdio.lb.h"
+
+FILE *fopen_fld(char *name, char *mode);
 
 int main(int argc, char *argv[])
 {
-    tis_init();
-    printf("%d", tisprint(';'));
-    printf("%d\n", tisprint(' '));
+    char a;
+    FILE *scroll = fopen_fld("a.txt","r");
+    for (int i =0; i<100; i++)
+        a=getc_fld(scroll);
+    
     return 0;
 }
