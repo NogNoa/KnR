40c3a2d1b8bd0677ea2f493f48a36d22acfcc1ab
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-10-06 19:14:58 +0300

    update numerals

--- cingles/numerals.lb.c
+++ cingles/numerals.lb.c
@@ -43,24 +43,22 @@ bcd* decimise(int call, bcd back[])
 
 bcd* normalise(bcd call, bcd back[2])
 {
-	back[1] = (bcd) {0,0};
-	while (call.eight > 1 || call.rest > 7)
-	{	if (call.eight > 1)
-			back[1].rest++, (call.eight-=2),(call.rest+=6);
-		else //if (call.rest > 7)
-			call.eight++,call.rest -= 8;
-	}
 	while (call.eight > 0 && call.rest > 1)
 		back[1].rest++, call.eight--, (call.rest-=2); 
 
-	return (bcd []) { {call.eight, call.rest}, back[1]};
+	return (back = (bcd []) { {call.eight, call.rest}, back[1]});
 }
 
 bcd* add_single(bcd adder, bcd addand, bcd back[2])
 {
 	int rsum, esum;
-
 	esum =adder.eight + addand.eight;
 	rsum = adder.rest + addand.rest;
-	return normalise((bcd) {esum, rsum},back);
+	while (esum > 1 || rsum > 7)
+	{	if (esum > 1)
+			back[1].rest++, (esum-=2),(rsum+=6);
+		else //if (rsum > 7)
+			esum++,rsum -= 8;
+	}
+	return (back = normalise((bcd) {esum, rsum},back));
 }
--- cingles/try.c
+++ cingles/try.c
@@ -5,5 +5,5 @@ int main(int argc, char *argv[])
 {
     bcd a[]={{1,7},{0,6}}, b[]={{0,3},{0,5}};
     bcd c[3];
-    printf("%d\n",binarise( add_single(a[1],b[1],c), 2 ));
+    printf("%d\n",binarise(add_single(a[1],b[1],c), 2 ));
 }
